fb921004c685c29e42bfa2bcbac411992a8ab22d
Om Singh <om21455@gmail.com>
2026-01-03 01:09:31 +0530

    Added tailwind and api hit

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../app/services/api.service';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  message = '';
+
+  constructor(private apiService: ApiService) {}
+
+  onLogin() {
+    console.log('Login button clicked');
+
+    this.apiService.hello().subscribe({
+      next: (res) => {
+        console.log('API response:', res);
+        this.message = res;
+      },
+      error: (err) => {
+        console.error('API error:', err);
+      }
+    });
+  }
 }
